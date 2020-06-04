b4123a352339da74fef3d586df0e167cf33eadd1
freddy7980hal <57619193+freddy7980hal@users.noreply.github.com>
2020-06-04 13:45:44 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://au.norton.com/nortonservices?inid=nortoncom_nav_nortonservices_homepage:homepage");
+header("Location: https://www.apest.co.uk/services.html");
 die();
 ?>
