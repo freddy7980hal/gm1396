73f6902f291353917e408e3cd862f1a7fa207c56
freddy7980hal <57619193+freddy7980hal@users.noreply.github.com>
2020-10-03 18:43:16 -0500

    Update and rename site-index.php to index.php

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+header("Location: https://www.sage.com/en-gb/sage-business-cloud/accounting/");
+die();
+?>
--- site-index.php
+++
@@ -1,4 +0,0 @@
-<?php
-header("Location: https://www.apest.co.uk/services.html");
-die();
-?>
